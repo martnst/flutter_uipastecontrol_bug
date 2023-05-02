5ab0a6e1cf41b0028962594016f7417657898f16
Martin Stemmle <hi@martn.st>
2023-05-02 18:29:42 +0200

    Reproduce prompt for user consent

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:uipastecontrol_bug/native_view_example.dart';
 
 void main() {
@@ -11,6 +12,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
+      theme: CupertinoThemeData(
+        // ⬇ 🔥 🔥 🔥 setting this property caused the app to prompt for user consent
+        barBackgroundColor: Colors.yellow,
+        // barBackgroundColor: null,
+      ),
       home: CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(),
         child: Center(
